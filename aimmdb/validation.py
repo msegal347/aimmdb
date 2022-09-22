558935370af4f7414d6283e3559766991db335b6
Joseph Kleinhenz <kleinhenz.joseph@gmail.com>
2022-09-22 09:29:52 -0700

    add validator function that only validates metadata

--- aimmdb/validation.py
+++ aimmdb/validation.py
@@ -4,6 +4,13 @@ from tiled.validation_registration import ValidationError
 from .schemas import BatteryChargeMetadata, ExperimentalXASMetadata
 
 
+def validate_xas_metadata(metadata, structure_family, structure, spec):
+    try:
+        metadata = ExperimentalXASMetadata.parse_obj(metadata)
+    except pydantic.ValidationError as e:
+        raise ValidationError(str(e))
+
+
 def validate_xas_tfy(metadata, structure_family, structure, spec):
     if structure_family != "dataframe":
         raise ValidationError(f"structure_family {structure_family} != dataframe")
@@ -13,10 +20,7 @@ def validate_xas_tfy(metadata, structure_family, structure, spec):
     if not {"energy", "i0", "tfy"}.issubset(columns):
         raise ValidationError(f"columns {columns} must contain i0 and tfy")
 
-    try:
-        metadata = ExperimentalXASMetadata.parse_obj(metadata)
-    except pydantic.ValidationError as e:
-        raise ValidationError(str(e))
+    validate_xas_metadata(metadata, structure_family, structure, spec)
 
 
 def validate_xas_tey(metadata, structure_family, structure, spec):
@@ -28,10 +32,7 @@ def validate_xas_tey(metadata, structure_family, structure, spec):
     if not {"energy", "i0", "tey"}.issubset(columns):
         raise ValidationError(f"columns {columns} must contain i0 and tey")
 
-    try:
-        metadata = ExperimentalXASMetadata.parse_obj(metadata)
-    except pydantic.ValidationError as e:
-        raise ValidationError(str(e))
+    validate_xas_metadata(metadata, structure_family, structure, spec)
 
 
 def validate_xas_transmission(metadata, structure_family, structure, spec):
@@ -43,10 +44,7 @@ def validate_xas_transmission(metadata, structure_family, structure, spec):
     if not {"energy", "i0", "itrans"}.issubset(columns):
         raise ValidationError(f"columns {columns} must contain i0 and itrans")
 
-    try:
-        metadata = ExperimentalXASMetadata.parse_obj(metadata)
-    except pydantic.ValidationError as e:
-        raise ValidationError(str(e))
+    validate_xas_metadata(metadata, structure_family, structure, spec)
 
 
 def validate_battery_charge_data(metadata, structure_family, structure, spec):
